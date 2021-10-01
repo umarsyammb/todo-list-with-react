cbce0008428f1da3cf977a77b892d7aaee90dab6
umarsyammb <umarsyammb@gmail.com>
2021-10-01 22:35:09 +0700

    ubah class menjadi className pada html

--- src/App.js
+++ src/App.js
@@ -2,49 +2,49 @@ import "./App.css";
 
 function App() {
 	return (
-		<div class="container">
-			<div class="frame">
-				<section class="header">
-					<button class="header-btn">Add</button>
-					<h1 class="header-title">Todo Lists</h1>
-					<button class="header-btn clear-red-btn">Clear</button>
+		<div className="container">
+			<div className="frame">
+				<section className="header">
+					<button className="header-btn">Add</button>
+					<h1 className="header-title">Todo Lists</h1>
+					<button className="header-btn clear-red-btn">Clear</button>
 				</section>
-				<section class="add">
-					<form class="add-form">
-						<input type="text" class="add-input" />
-						<button class="add-btn">Add</button>
+				<section className="add">
+					<form className="add-form">
+						<input type="text" className="add-input" />
+						<button className="add-btn">Add</button>
 					</form>
 				</section>
-				<section class="todos">
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+				<section className="todos">
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
-					<div class="todo">
-						<span class="todo-text">Learning React</span>
+					<div className="todo">
+						<span className="todo-text">Learning React</span>
 					</div>
 				</section>
 			</div>
