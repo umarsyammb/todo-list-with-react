c4fa6d1e501ccd707b8ebca32f5d7fcd8df7b687
umarsyammb <umarsyammb@gmail.com>
2021-10-03 11:10:14 +0700

    refactor Todos

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,38 +1,19 @@
 import React from "react";
+import Todo from "./Todo";
 
 const Todos = () => {
+	const todos = [
+		{ text: "Belajar React" },
+		{ text: "Belajar Props" },
+		{ text: "Belajar Hooks" },
+		{ text: "Belajar State" },
+		{ text: "Belajar Layouting" },
+	];
 	return (
 		<section className="todos">
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
-			<div className="todo">
-				<span className="todo-text">Learning React</span>
-			</div>
+			{todos.map((todo) => {
+				return <Todo text={todo.text} />;
+			})}
 		</section>
 	);
 };
