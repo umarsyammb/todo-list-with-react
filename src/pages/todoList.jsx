a29141fbb7de6ced899477287b1c8a2615a489b5
umarsyammb <umarsyammb@gmail.com>
2021-10-03 10:40:16 +0700

    menambahkan folder baru

---
+++ src/pages/todoList.jsx
@@ -0,0 +1,16 @@
+import Paper from "../components/Paper";
+import Header from "../components/Header";
+import TodoForm from "../components/TodoForm";
+import Todos from "../components/Todos";
+
+const TodoList = () => {
+	return (
+		<Paper>
+			<Header />
+			<TodoForm />
+			<Todos />
+		</Paper>
+	);
+};
+
+export default TodoList;
